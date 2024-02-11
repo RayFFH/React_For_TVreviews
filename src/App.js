b1019d59355f0666c33fe44940ac4f00810404a1
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-02-11 21:43:05 +0000

    Fixing hashrouter

--- src/App.js
+++ src/App.js
@@ -62,8 +62,8 @@ function App() {
     <div className="App">
       <Header/>
       <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Home movies={movies} />} ></Route>
+          <Route path="/React_For_TVreviews" element={<Layout/>}>
+            <Route path="/React_For_TVreviews" element={<Home movies={movies} />} ></Route>
             <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
             <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
             <Route path="*" element = {<NotFound/>}></Route>
