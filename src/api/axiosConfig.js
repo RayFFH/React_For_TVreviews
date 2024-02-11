9ccf7c271db3c16ec091ee2596a9739b00999837
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-02-11 02:55:47 +0000

    redeploying

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'localhost:8080/',
-
-});
-    
-    //headers: {"ngrok-skip-browser-warning": "true"}
-    api.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://rayffh.github.io/React_For_TVreviews/';
-
-
-    export default api;
-
+    baseURL: 'https://tvreviews-1707597591977.azurewebsites.net',
+  });
+  
+  // Add CORS headers
+  api.defaults.headers.common['Access-Control-Allow-Origin'] = 'localhost:3000';
+  
+  export default api;
